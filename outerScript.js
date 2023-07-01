a68eadcc96a2a95ba78a1dbb3f044f1cc4f0e42c
NyleLuza <Jluza21@gmail.com>
2023-06-30 22:58:40 -0700

    added row on right lower section, dynamically updates balances and adds description of purchase

--- outerScript.js
+++ outerScript.js
@@ -1,16 +1,22 @@
 
 const expensesButton = document.getElementById("btnE");
 const purchaseAmount = document.getElementById("purchase");
-const purchaseDesc = document.getElementById("purchase1");
+let purchaseDesc = document.getElementById("purchase1");
 let expensesAmount = localStorage.getItem('needs-Amount');
+let overallBalance = localStorage.getItem('overallB');
 const numText = document.getElementById("numberText");
 const descText = document.getElementById("descriptionText");
-let updatingBalance = document.getElementsByClassName("currentBalance");
+let updatingBalance = document.getElementById("currentBalance");
+let expensesBalance = document.getElementById("expensesBalance")
+
+    updatingBalance.innerText = ' $'+ overallBalance;
+    expensesBalance.innerText = ' $'+ expensesAmount;
 expensesButton.addEventListener('click',function(e){
     e.preventDefault();
-     expensesAmount = expensesAmount - parseFloat(purchaseAmount.value);
+    expensesAmount = expensesAmount - parseFloat(purchaseAmount.value);
+    updatingBalance.innerText = '$' + (overallBalance - purchaseAmount.value);
     let nT = createEle(expensesAmount);
-    let dT = createText(purchaseDesc.value);
+    let dT = createText(purchaseDesc);
     numText.append(nT);
     descText.append(dT);
     
@@ -26,6 +32,6 @@ function createEle(amount) {
   function createText(description){
     let descriptionBox = document.createElement("div");
     descriptionBox.setAttribute("id", "userDescription")
-    descriptionBox.innerText = description;
+    descriptionBox.innerText = description.value;
     return descriptionBox;
   }
